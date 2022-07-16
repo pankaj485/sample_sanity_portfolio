91ea654687d53f18ecd025cce806b65e4c4b5698
pankaj485 <pankajsamar2@gmail.com>
2022-07-16 20:06:57 +0530

    style primary navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,47 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
+  const defaultStyles =
+    "py-3 px-6 my-3 mr-4 rounded text-red-200 hover:text-green-800";
   return (
-    <header>
-      <div>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="about">About</Link>
-          <Link to="post">post</Link>
-          <Link to="project">project</Link>
+    <header className="bg-red-600">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex items-center mx-14">
+          <NavLink
+            to="/"
+            className={(link) =>
+              "py-6 px-3  text-4xl font-bold cursive " +
+              defaultStyles +
+              (!link.isActive ? "" : "text-white")
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="about"
+            className={(link) =>
+              defaultStyles + (!link.isActive ? "" : "text-red-100 bg-red-700")
+            }
+          >
+            About
+          </NavLink>
+          <NavLink
+            to="post"
+            className={(link) =>
+              defaultStyles + (!link.isActive ? "" : "text-red-100 bg-red-700")
+            }
+          >
+            post
+          </NavLink>
+          <NavLink
+            to="project"
+            className={(link) =>
+              defaultStyles + (!link.isActive ? "" : "text-red-100 bg-red-700")
+            }
+          >
+            project
+          </NavLink>
         </nav>
       </div>
     </header>
