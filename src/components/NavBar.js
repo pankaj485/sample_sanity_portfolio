9691484657e3c7340e4e2582d9e59f191908d9b0
pankaj485 <pankajsamar2@gmail.com>
2022-07-16 20:14:53 +0530

    add social icons on navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 const NavBar = () => {
   const defaultStyles =
     "py-3 px-6 my-3 mr-4 rounded text-red-200 hover:text-green-800";
   return (
     <header className="bg-red-600">
-      <div className="container mx-auto flex justify-between">
-        <nav className="flex items-center mx-14">
+      <div className="container px-40 mx-auto flex justify-between ">
+        <nav className="flex items-center">
           <NavLink
             to="/"
             className={(link) =>
@@ -43,6 +44,26 @@ const NavBar = () => {
             project
           </NavLink>
         </nav>
+        <div className="inline-flex p-3 my-6">
+          <SocialIcon
+            url="https://twitter.com/"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff style={{height: 35, width: 35}} "
+          />
+          <SocialIcon
+            url="https://facebook.com"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff style={{height: 35, width: 35}} "
+          />
+          <SocialIcon
+            url="https://instagram.com"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff style={{height: 35, width: 35}} "
+          />
+        </div>
       </div>
     </header>
   );
