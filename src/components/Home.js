167975b38dc6a9214e4bfdad6226c7ec8a36fe4a
pankaj485 <pankajsamar2@gmail.com>
2022-07-16 21:06:50 +0530

    add homepage contents

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,20 @@
 import React from "react";
-
+import image from "../assets/profile_photo.png";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main>
+      <img
+        src={image}
+        alt="person's profile pic"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative flex justify-center items-center max-h-screen pt-24 lg:pg-64 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+          Hi, I'm Pank
+        </h1>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
