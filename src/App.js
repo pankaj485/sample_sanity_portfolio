728c16a9c062d8c2db507906c5ec47eb7814875a
pankaj485 <pankajsamar2@gmail.com>
2022-07-16 16:44:31 +0530

    add basic routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import * as React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
+import Home from "./components/Home";
+import About from "./components/About.js";
+import SinglePost from "./components/SinglePost";
+import Post from "./components/Post";
+import Project from "./components/Project";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="post/:slug" element={<SinglePost />} />
+        <Route path="post" element={<Post />} />
+        <Route path="project" element={<Project />} />
+      </Routes>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <header>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="about">About</Link>
+          <Link to="post">post</Link>
+          <Link to="project">project</Link>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default NavBar;
