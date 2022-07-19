e3d3b2aeec23cd4a86522f5d7730e143c7d66243
pankaj485 <pankajsamar2@gmail.com>
2022-07-19 09:00:52 +0530

    create own UI for projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import SanityClient from "../client";
 
 const Project = () => {
-  return <div>Project</div>;
+  const [projects, setProjects] = useState(null);
+
+  useEffect(() => {
+    SanityClient.fetch(
+      `*[_type == "project"]{
+        date, 
+        description,
+        link, 
+        place,
+        projectType, 
+        tags,
+        title, 
+}`
+    )
+      .then((data) => setProjects(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  console.log(projects);
+
+  return (
+    <main>
+      <section>
+        <h1>Projects </h1>
+
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 mx-5 gap-5">
+          {projects?.map((project, index) => {
+            const { title, description, date, link, place, projectType, tags } =
+              project;
+            return (
+              <div key={index} className="border-2 border-blue-400 p-4 ">
+                <h2 className="text-xl font-bold">{title}</h2>
+                <h3 className="text-lg">{description}</h3>
+                <br />
+                <p>category: {projectType}</p>
+                <a href={link}>{link}</a>
+                <p className="mb-2">
+                  date: {date}, location: {place}
+                </p>
+                {tags.map((tag, index) => (
+                  <span
+                    key={index}
+                    className="bg-red-200 mr-2 text-sm px-4 py-1 rounded-full"
+                  >
+                    {tag + (index < tags.length - 1 ? ", " : "")}
+                  </span>
+                ))}
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Project;
